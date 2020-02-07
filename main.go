43830f867fb7f1fb9507141d3ff9b73712235f7e
liuquanal <liuquanal@sina.com>
2020-02-07 16:06:47 +0800

    修改打印信息

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func myIpHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,13 +17,16 @@ func myIpHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	version := "1.0.0"
 	port := "12345" // 端口号
 	ht := http.HandlerFunc(myIpHandler)
 	if ht != nil {
 		http.Handle("/myip", ht)
 	}
+	fmt.Println("version: " + version)
 	fmt.Println("service is running on port: " + port)
 	fmt.Println("访问http://127.0.0.1:" + port + "/myip 查看您的IP地址")
+	fmt.Println("启动于：" + time.Now().Format("2006-01-02 15:04:05"))
 	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err.Error())
